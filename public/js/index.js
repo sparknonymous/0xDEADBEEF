6c461e337649bd4a098e9729b6bcca3d3b9a86be
Phillip Zhou <pzhou53@gmail.com>
2017-04-25 14:24:42 -0700

    Reformatting code and moving code to JS files.

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,52 @@
-$("footer > tab").click(function () {
+$("footer > tab").click(function() {
     $(this).addClass("active").siblings().removeClass("active");
     $("#" + $(this).attr("id") + "-section").addClass("active").siblings().removeClass("active");
 });
 
-$("#found-form").submit(function () {
+$("#found-form").submit(function() {
     postFound();
     return false;
 });
 
 function ajax(option) {
     var originalSuccess = option.success;
-    option.success = function (result) {
+    option.success = function(result) {
         var data = JSON.parse(result);
         if (data.code == 0) {
             originalSuccess(data.content);
-        }
-        else {
+        } else {
             alert("Error " + data.code + ": " + data.msg);
         }
     };
-    option.error = function () {
+    option.error = function() {
         alert("Internet connection error");
     };
     $.ajax(option);
+}
+
+var map;
+
+function initMap() {
+    $("#map-loading").fadeOut();
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 0.0,
+            lng: 0.0
+        },
+        zoom: 15
+    });
+
+    if (navigator.geolocation) {
+        getGeoPosition();
+    }
+}
+
+function getGeoPosition() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var currPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        map.setCenter(currPosition);
+    });
 }
